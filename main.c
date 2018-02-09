1a5e1c2734ae97063eaaa9d899f58390e3c782f4
mnmoretti <manuel.moretti@tiscali.it>
2018-02-09 23:04:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,66 @@
 #include <stdio.h>
-#include "mylib.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "gamelib.c"
 
-/* Questo file contiene solo la stampa del menu principale e poi richiama una delle
- tre funzioni possibili: crea_mappa(), gioca(), termina_gioco(). */
+int main(){
 
+menu();
 
-int main (void) {
-    
-    
-    
+}
+
+int menu(){
+
+    char choice;
+    bool valid = false;
+
+    while (!valid) {
+
+      printf("\n _______________\n|               |\n| [1]CREA MAPPA |\n| [2]GIOCA      |\n| [3]ESCI       |\n|_______________|\n\n");
+      scanf("%c", &choice);
+
+      switch (choice) {
+
+        case '1':
+
+          system("clear");
+          valid = true;
+          gest_map();
+
+        break;
+
+        case '2':
+
+          system("clear");
+          valid = true;
+          inizializzaRand();
+          inizializza_partita(numero_giocatori);
+          printf("avvio gioco...\n");
+          gioca();
+
+        break;
+
+        case '3':
+
+          system("clear");
+          printf("chiusura in corso...\n");
+          valid = true;
+          gamelib.termina_gioco();
+
+        break;
+
+        default:
+
+          printf("Scelta non disponibile\n");
+          sleep(1);
+          system("clear");
+          menu();
+
+        break;
+      }
+
+}
+
 }
